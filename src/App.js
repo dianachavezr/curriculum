7c762f3ae47b306f508aa76cbf6371bf3ccfd6a5
DIANACH <dianepatrich@gmail.com>
2021-06-16 20:17:03 -0500

    Se agrega el proyecto modulo 4 Curriculum Diana Chavez

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Cajaizquierda from "./cajaizquierda";
+import Acerca from "./acerca";
+import Educacion from "./educacion";
+import {BrowserRouter as Router,Switch,Route,Link, Navlink, Redirect} from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <div>
+    <Router>
+      <section id="informacion" >
+     <Cajaizquierda/>
+              <Switch>
+          <Route exact path="/acerca">
+            <Acerca/>
+            </Route>
+            <Route exact path="/" render={() => (<Redirect to="/acerca"/>)}/>
+          <Route exact path="/educacion">
+          <Educacion/>
+          </Route>
+       
+      
+        </Switch>
+       
+      </section>
+    </Router>
+
+        </div>
+
+
   );
 }
 
